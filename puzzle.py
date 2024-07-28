a13de1efd0f91d538947c513934ec3f5bf35dc9f
Jocemar Nicolodi Junior <jnicolodijunior@gmail.com>
2024-07-28 09:08:07 -0300

    knoledge0

--- puzzle.py
+++ puzzle.py
@@ -9,24 +9,55 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+ALied = Symbol("A Lied")
+
+ASaysIsKnightandKnave = ALied
+
+
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Each person is either a knight or a knave.
+    Or(AKnight, AKnave),
+    # If a person is a Knight, it is not a knave
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(ALied, Not(AKnight)),
+    # If a person is a Knave, it is not a knight
+    Implication(ASaysIsKnightandKnave, ALied),
+    # If lies, is a knave
+    ASaysIsKnightandKnave
 )
+
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+  # Each person is either a knight or a knave.
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+
+    # If a person is a Knight, it is not a knave \
+    Implication(AKnight, Not(AKnave)),
+    Implication(BKnight, Not(BKnave)),
+
+    # If a person is a Knave, it is not a knight 
+    Implication(AKnave, Not(AKnight)),
+    Implication(BKnave, Not(BKnight)),
+
+    #
+
 )
+
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+
 knowledge2 = And(
-    # TODO
+  AKnave, BKnight
 )
 
 # Puzzle 3
@@ -35,7 +66,7 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    
 )
 
 
